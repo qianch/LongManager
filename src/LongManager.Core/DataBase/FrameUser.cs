1bd1ae205124433c08ccc30c214ac0cd549bf4db
Ellis <qianchenchn@foxmail.com>
2019-01-30 19:58:43 +0800

    修改GlobalCache

--- src/LongManager.Core/DataBase/FrameUser.cs
+++ src/LongManager.Core/DataBase/FrameUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -7,16 +8,61 @@ using System.Text;
 namespace LongManager.Core.DataBase
 {
     [Table("FrameUser")]
-    public class FrameUser
+    public class FrameUser : INotifyPropertyChanged
     {
+        private int _id;
+        private string _rowGuid;
+        private string _userName;
         [Key]
-        public int ID { get; set; }
-        public string RowGuid { get; set; }
-        public string UserName { get; set; }
+        public int ID
+        {
+            get { return _id; }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    Notify("ID");
+                }
+            }
+        }
+
+        public string RowGuid
+        {
+            get { return _rowGuid; }
+            set
+            {
+                if (value != RowGuid)
+                {
+                    _rowGuid = value;
+                    Notify("RowGuid");
+                }
+            }
+        }
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                if (value != _userName)
+                {
+                    _userName = value;
+                    Notify("UserName");
+                }
+            }
+        }
         public string Password { get; set; }
         public string DisplayName { get; set; }
         public string Mobile { get; set; }
         public string Address { get; set; }
         public string Birthday { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Notify(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- src/LongManager.Core/GlobalCache.cs
+++ src/LongManager.Core/GlobalCache.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace LongManager.Core
 {
@@ -9,6 +11,14 @@ namespace LongManager.Core
     {
         public static GlobalCache Instance { get; } = new GlobalCache();
         public Dictionary<string, Page> AllPages { get; } = new Dictionary<string, Page>();
-        public Frame Frame { get; set; }
+        public Frame Frame { get; set; } = new Frame();
+        public NavigationWindow NavigationWindow { get; set; } = new NavigationWindow
+        {
+            ShowsNavigationUI = false,
+            Width = 800,
+            Height = 450,
+            ShowInTaskbar = false,
+            ResizeMode = ResizeMode.NoResize
+        };
     }
 }
