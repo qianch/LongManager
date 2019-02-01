64c7c5a26a72be68b783eeed0fbd039942c578e3
Ellis <qianchenchn@foxmail.com>
2019-02-01 16:56:11 +0800

    cefsharp 前后台相互调用，新增 layer.js

--- src/LongManager/Pages/Welcome.xaml.cs
+++ src/LongManager/Pages/Welcome.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.Wpf;
+using LongManager.Core.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +27,42 @@ namespace LongManager.Pages
         public Welcome()
         {
             InitializeComponent();
+
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+            var bindingOptions = new BindingOptions()
+            {
+                Binder = BindingOptions.DefaultBinder.Binder,
+                // MethodInterceptor = new MethodInterceptorLogger()
+            };
+            Browser.RegisterAsyncJsObject("jsObject", new CallbackObjectForJs(), bindingOptions);
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            Browser.Address = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Htmls", "map.html");
+            Browser.Address = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Htmls/pages/map.html");
+        }
+
+        private void CallBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            Browser.ExecuteScriptAsync("browserAlert", "browserAlert");
+        }
+
+        private void CallBrowserGet_Click(object sender, RoutedEventArgs e)
+        {
+            Task<CefSharp.JavascriptResponse> javascriptResponse = Browser.EvaluateScriptAsync("browserAlertReturn", "browserAlertReturn");
+            if (javascriptResponse.Result.Success)
+            {
+                MessageBox.Show(javascriptResponse.Result.Result.ToString());
+            }
+        }
+    }
+
+    public class CallbackObjectForJs
+    {
+        public string name = "";
+        public void showMsg(string msg)
+        {
+            MessageBox.Show(msg);
         }
     }
 }
