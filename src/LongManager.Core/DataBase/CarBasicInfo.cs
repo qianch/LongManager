4864ba85d4c7dc25082a951d352d99a5f5655eb9
Ellis <qianchenchn@foxmail.com>
2019-02-13 22:33:50 +0800

    新增CarBasicInfo

---
+++ src/LongManager.Core/DataBase/CarBasicInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace LongManager.Core.DataBase
+{
+    [Table("CarBasicInfo")]
+    public class CarBasicInfo
+    {
+        [Key]
+        public int ID { get; set; }
+        public string RowGuid { get; set; }
+        public string CarNO { get; set; }
+    }
+}
