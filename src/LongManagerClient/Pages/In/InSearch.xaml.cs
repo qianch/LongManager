09dc69798703e48e1bbadd76b5247a5a3215db8d
qianchenchn <qianchenchn@foxmail.com>
2019-11-16 22:04:25 +0800

    进口，出口都调整为8万

--- src/LongManagerClient/Pages/In/InSearch.xaml.cs
+++ src/LongManagerClient/Pages/In/InSearch.xaml.cs
@@ -28,7 +28,7 @@ namespace LongManagerClient.Pages.In
         private string _login = "";
         private string _logout = "";
         private string _inMail = "";
-        private const int _lastPage = 500;
+        private const int _lastPage = 400;
         private const int _pageSize = 200;
         private string _date = "";
         private int _currentPage = 0;
